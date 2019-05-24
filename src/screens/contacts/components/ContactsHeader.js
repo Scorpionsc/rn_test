4206be0c0871ce002db7af4624ffcd04c79eeecc
Mykola Petrunin <petruninnikolay@gmail.com>
2019-05-24 23:24:07 +0300

    add some fixes

--- src/screens/contacts/components/ContactsHeader.js
+++ src/screens/contacts/components/ContactsHeader.js
@@ -6,9 +6,9 @@ import IosArrowBack from 'react-ionicons/lib/IosArrowBack'
 class ContactsHeader extends React.PureComponent {
     render() {
         return (<header className="contactsHeader">
-            <IosArrowBack className="contactsHeader__iconBack" fontSize="20px" color="#6D6D6D"/>
+            <IosArrowBack className="contactsHeader__iconBack" fontSize="22px" color="#6D6D6D"/>
             <h1 className="contactsHeader__title">Контакты</h1>
-            <IosSettingsOutline className="contactsHeader__iconSettings" fontSize="20px" color="#6D6D6D"/>
+            <IosSettingsOutline className="contactsHeader__iconSettings" fontSize="22px" color="#6D6D6D"/>
         </header>);
     }
 }
